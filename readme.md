e7a5e7e5e57f29c8ddc21e0f616c5a43877a45c4
valeria borisova <valeria.borisova@iqoption.com>
2018-03-19 17:33:55 +0300

    Update readme Update eslint

--- readme.md
+++ readme.md
@@ -8,9 +8,8 @@ Install node modules
 	
 Start the server
 
-	npm start
-	// Or for Webpack Dashboard
 	npm run dev
+
 	
 
 ## Production build
@@ -23,40 +22,3 @@ This will build a uglified `app-[hash].js` and a minified `app-[hash].css` and a
 
 The `build/` directory is `.gitignore`'d by default, and purged before every build.
 
-## Deployment
-
-We can deploy to [now](https://github.com/zeit/now-cli) ([zeit](https://zeit.co/)) right out the box. Ensure you have the [now-cli](https://www.npmjs.com/package/now) installed globally with npm first.
-
-Edit the `now.json` in the root, change this to the name of your app, then run:
-
-	npm run deploy
-	
-This will build the production assets and deploy to now. 
-
-To alias your deployment (e.g. `my-app.example.com` or `my-app.now.sh`) supply your `alias` in the `now.json` and run:
-
-	npm run alias
-	
-Your deployment should now be aliased.
-
-If you want to deploy to now for production also, then you will likely have a slightly different config for production (i.e. a different `alias` pointing to your live domain). There is a supplied `now.production.json` config for this. To deploy/alias to production run:
-
-	npm run deploy-production
-	npm run alias-production
-
-## Available commands
-
-- `npm start` - start the dev server
-- `npm run clean` - delete the `build` folder
-- `npm run lint` - run a eslint check
-- `npm test` - run all tests
-- `npm run dev` - start the dev server using webpack dashboard
-- `npm run build` - create a production ready build in the `build` folder
-- `npm run deploy` - deploy the production build to [now](https://github.com/zeit/now-cli)
-- `npm run alias` - alias the deployed build on [now](https://github.com/zeit/now-cli)
-- `npm run deploy-production` - deploy the production build to a production ready [now](https://github.com/zeit/now-cli) instance
-- `npm run alias-production` - alias the deployed build on a production ready [now](https://github.com/zeit/now-cli) instance
-
-
-# Select
-# Select
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -11,110 +11,108 @@ const HappyPack = require('happypack');
 
 const happyThreadPool = HappyPack.ThreadPool({ size: 5 });
 
-
-
 module.exports = {
 
-	devtool: false,
+  devtool: false,
 
-	context: resolve(__dirname, 'src'),
-	entry: './index',
-	output: {
-		filename: 'app.[hash].js',
-		path: resolve(__dirname, 'build'),
-		publicPath: './',
-	},
+  context: resolve(__dirname, 'src'),
+  entry: './index',
+  output: {
+    filename: 'app.[hash].js',
+    path: resolve(__dirname, 'build'),
+    publicPath: './',
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				loaders: ['babel-loader'],
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: 'css-loader'
-				})
-			},
-			{
-				test: /\.styl$/,
-				loader: 'happypack/loader?id=styl',
-			},
-			{ test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=[hash:base64:5].[ext]' },
-			{ test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader' },
-			{
-				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				use: 'url-loader?limit=10000&mimetype=application/font-woff'
-			},
-			{ test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' }
-		]
-	},
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.styl$/,
+        loader: 'happypack/loader?id=styl',
+      },
+      { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=[hash:base64:5].[ext]' },
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader' },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+    ]
+  },
 
-	resolve: {
-		extensions: ['.js', '.jsx', '.styl'],
-		alias: {
-			img:resolve(__dirname, 'src/public/img'),
-			components: resolve(__dirname, 'src/components'),
-			public: resolve(__dirname, 'src/public'),
-			ducks: resolve(__dirname, 'src/redux/ducks'),
-			libs: resolve(__dirname, 'src/libs'),
-		}
-	},
+  resolve: {
+    extensions: ['.js', '.jsx', '.styl'],
+    alias: {
+      img: resolve(__dirname, 'src/public/img'),
+      components: resolve(__dirname, 'src/components'),
+      public: resolve(__dirname, 'src/public'),
+      ducks: resolve(__dirname, 'src/redux/ducks'),
+      libs: resolve(__dirname, 'src/libs'),
+    }
+  },
 
-	plugins: [
-		new webpack.optimize.ModuleConcatenationPlugin(),
-		new HtmlWebpackPlugin({
-			template: `${__dirname}/src/index.html`,
-			filename: 'index.html',
-			inject: 'body',
-		}),
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production'),
-			}
-		}),
-		new HappyPack({
-			id: 'styl',
-			threadPool: happyThreadPool,
-			loaders: ['style-loader', 'css-loader', 'stylus-loader']
-		}),
-		new webpack.optimize.OccurrenceOrderPlugin(),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true,
-			debug: true,
-			options: {
-				context: __dirname,
-				stylus: {
-					import: `${__dirname}/src/styles/global.styl`,
-					use: [
-						rupture(),
-						autoprefixer({
-							browsers: ['last 3 versions']
-						})
-					]
-				}
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false,
-				screw_ie8: true,
-				conditionals: true,
-				unused: true,
-				comparisons: true,
-				sequences: true,
-				dead_code: true,
-				evaluate: true,
-				if_return: true,
-				join_vars: true,
-			},
-			comments: false
-		}),
-		new ExtractTextPlugin({ filename: 'app-[hash].css', disable: false, allChunks: true })
-	]
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      }
+    }),
+    new HappyPack({
+      id: 'styl',
+      threadPool: happyThreadPool,
+      loaders: ['style-loader', 'css-loader', 'stylus-loader']
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: true,
+      options: {
+        context: __dirname,
+        stylus: {
+          import: `${__dirname}/src/styles/global.styl`,
+          use: [
+            rupture(),
+            autoprefixer({
+              browsers: ['last 3 versions']
+            })
+          ]
+        }
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      comments: false
+    }),
+    new ExtractTextPlugin({ filename: 'app-[hash].css', disable: false, allChunks: true })
+  ]
 
 }
